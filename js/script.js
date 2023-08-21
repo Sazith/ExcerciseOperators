011ba8462f563bf43f08c29f9291917d0de7d809
Md. Sazith Shyonton <sazith.simecsoftdev@gmail.com>
2023-08-21 16:54:06 +0600

    Task Completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 console.log("Hello JS !");
+
+var temp = prompt("Please enter temperature : "); 32
+var result = 9 / 5 * temp + 32;
+alert(" Farenheit : " + result + " Degree ");
+console.log(" Task Completed ");
